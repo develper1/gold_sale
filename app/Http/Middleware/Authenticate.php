1ac061aaaf9193febb4cd18987d9799ce7e6b123
Taleem Hussain <61445876+taleemhussain@users.noreply.github.com>
2025-07-11 18:33:34 +0500

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,15 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($request->expectsJson()) {
+            return null;
+        }
+    
+        // Check if the request is for an admin route
+        if ($request->is('admin*')) {
+            return route('admin.login');
+        }
+
+        return route('login');
     }
 }
