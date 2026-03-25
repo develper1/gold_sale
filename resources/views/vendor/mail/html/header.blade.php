558355b5680cf033234e5c68f9ccdbbf1ab88b7c
taleem35 <taleem35@gmail.com>
2026-03-25 15:36:54 +0500

    email logo and input size for forget and the reset pages

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -6,6 +6,7 @@
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 <img src="{{ asset('assets/media/logo.png') }}" class="logo" alt="{{ config('app.name') }}">
+<!-- <img src="{{ asset('assets/media/logo.png') }}" class="logo" alt="{{ config('app.name') }}" width="180" style="display:block;margin:0 auto;max-width:180px;height:auto;"> -->
 @endif
 </a>
 </td>
